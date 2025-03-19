37be33cf6cac309903cb12253c48bb8400be8b94
danielabouassalyy <abou0344@algonquinlive.com>
2025-03-18 20:26:14 -0400

    Lab 11: Add accessible accordion with ARIA and keyboard navigation

--- accordion.js
+++ accordion.js
@@ -1,30 +1,62 @@
-//pseudocode
-/*
-  1.Grab the accordion buttons from the DOM
-  2. go through each accordion button one by one
-  3. Use the classlist dom method in combination with the toggle method provided by the DOM to add or remove the "is-open" class. At this point, the accordion button should be able to switch back and forth between its font awesome icons but there is no content inside of it. This is because of the overflow:hidden and the max-height of zero; it is hiding our content. So now we must use javascript to change these values with DOM CSS
-  4. get the div that has the content of the accordion button you are currently looking at; we do this using the .nextElementSibling method which allows us to look at the html element that is directly next to the current html element we are looking at. Since we are currently looking at a button (accordion button), the next element after that is the div with the class accordion-content. This is exactly what we want because it allows us to work with the div that has the content that we want to display. Also please note that we could have got to this div in another way but this is the "shortest path" to our answer.
-  
-  5. set the max-height based on whether the current value of the max-height css property. If the max-height is currently 0 (if the page has just been visited for the first time) or null (if it has been toggled once already) which means that it is closed, you will give it an actual value so the content will be shown; if not then that means the max-height currently has a value and you can set it back to null to close it.
-  6. If the accordion is closed we set the max-height of the currently hidden text inside the accordion from 0 to the scroll height of the content inside the accordion. The scroll height refers to the height of an html element in pixels. For this specific example, we are talking about the height of the div with the class accordion-content with all of its nested ptags
-*/
-
+// Grab all buttons
 const accordionBtns = document.querySelectorAll(".accordion");
 
-accordionBtns.forEach((accordion) => {
-  accordion.onclick = function () {
+// loop each
+accordionBtns.forEach(function (accordion, index) {
+
+  // on click
+  accordion.addEventListener("click", function () {
+    // Toggle 
     this.classList.toggle("is-open");
 
+    //  Get the corresponding content 
     let content = this.nextElementSibling;
-    console.log(content);
 
+    // Check if the accordion is open 
     if (content.style.maxHeight) {
-      //this is if the accordion is open
+      // If maxHeight is set, collapse 
       content.style.maxHeight = null;
+      // ARIA  collapse
+      this.setAttribute("aria-expanded", "false");
     } else {
-      //if the accordion is currently closed
+      // If the accordion is closed, expand
       content.style.maxHeight = content.scrollHeight + "px";
-      console.log(content.style.maxHeight);
+      //  ARIA expande
+      this.setAttribute("aria-expanded", "true");
     }
-  };
+  });
+
+  // Keydown event
+  accordion.addEventListener("keydown", function (event) {
+    //  ArrowDown, move focus to the next accordion button
+    if (event.key === "ArrowDown") {
+      let nextIndex = index + 1;
+      // Wrap 
+      if (nextIndex >= accordionBtns.length) {
+        nextIndex = 0;
+      }
+      accordionBtns[nextIndex].focus();
+      event.preventDefault(); // (Prevent default scrolling behavior)
+    }
+    //  ArrowUp, move focus to the previous accordion button
+    else if (event.key === "ArrowUp") {
+      let prevIndex = index - 1;
+      // Wrap 
+      if (prevIndex < 0) {
+        prevIndex = accordionBtns.length - 1;
+      }
+      accordionBtns[prevIndex].focus();
+      event.preventDefault();
+    }
+    // If Home key is pressed, focus the first button
+    else if (event.key === "Home") {
+      accordionBtns[0].focus();
+      event.preventDefault();
+    }
+    // If End key is pressed, focus the last  button
+    else if (event.key === "End") {
+      accordionBtns[accordionBtns.length - 1].focus();
+      event.preventDefault();
+    }
+  });
 });
